a90a1aec9fdc1bfd0999e1d09f8e88dc23fa1dc9
emran <emranhi001@gmail.com>
2024-08-06 07:09:20 +0300

    merged task 2.1 and 2.2

--- Task-2-word_freq&palindrome.go
+++ Task-2-word_freq&palindrome.go
@@ -4,7 +4,30 @@ import (
 	"fmt"
 	"strings"
 	"unicode"
+	
 )
+
+func WordFreqCounter(word string) map[string]int64{
+	words := strings.Fields(word)
+	counter := make(map[string]int64)
+
+	for _, word:= range words{
+		var clean_word string
+		for _, chr:= range word{
+			if unicode.IsLetter(chr){
+				clean_word += string(chr)
+			}
+		}
+		if clean_word != ""{
+			counter[clean_word] += 1
+		}
+	}
+
+	fmt.Println(counter)
+
+	return counter
+}
+
 
 func PalindromeCheck(word string){
 	rstr := []rune(strings.ToLower(word))
